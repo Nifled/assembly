a042bb9795b3b645aff0597e2d2e5947acc87ce1
Erick <erickdelfinmolina@gmail.com>
2017-03-29 17:02:35 -0700

    Added new functions to master functions.asm

--- 2.- functions/functions.asm
+++ 2.- functions/functions.asm
@@ -11,50 +11,50 @@ stdout EQU 1
 stderr EQU 3
 
 strlen:
-    push EBX
-    mov EBX, EAX
+    push ebx
+    mov ebx, eax
 
 sigcar:
-    cmp byte[EAX], 0                                 ;checks if curr byte == 0
+    cmp byte[eax], 0                                 ;checks if curr byte == 0
     jz finalizar                                     ;if 0, jump to finalizar
-    inc EAX                                          ;else, incremente EAX
+    inc eax                                          ;else, incremente eax
     jmp sigcar
 
 finalizar:
-    sub EAX, EBX
-    pop EBX
+    sub eax, ebx
+    pop ebx
     ret
 
 sprint:
-    push EDX                                         ;save value of EDX
-    push ECX                                         ;save value of ECX
-    push EBX                                         ;save value of EBX
-    push EAX                                         ;save value of EAX
+    push edx                                         ;save value of edx
+    push ecx                                         ;save value of ecx
+    push ebx                                         ;save value of ebx
+    push eax                                         ;save value of eax
     call strlen                                      ;call strlen function
 
-    mov EDX, EAX                                     ;move len of msg
-    pop EAX                                          ;get EAX location from stack
-    mov ECX, EAX                                     ;msg's address
-    mov EBX, stdout                                       ;stdout
-    mov EAX, sys_write                                       ;sys_write
+    mov edx, eax                                     ;move len of msg
+    pop eax                                          ;get eax location from stack
+    mov ecx, eax                                     ;msg's address
+    mov ebx, stdout                                       ;stdout
+    mov eax, sys_write                                       ;sys_write
     int 80h                                          ;executes
 
-    pop EBX                                          ;re-establish EBX
-    pop ECX                                          ;re-establish ECX
-    pop EDX                                          ;re-establish EDX
+    pop ebx                                          ;re-establish ebx
+    pop ecx                                          ;re-establish ecx
+    pop edx                                          ;re-establish edx
     ret
 
 ;Function sprintLF,	prints with LineFeed (new line)	
 sprintLF:	
 	call sprint		                                 ;calls and prints msg
 	
-	push EAX		                                 ;save accumulator EAX to use in function
-	mov EAX,0xA		                                 ;Hexadecimal LineFeed character
-	push EAX		                                 ;save 0xA to stack
-	mov EAX, ESP	                                 ;mov 0xA location to stack  // ESP points to last direction of stack or to EAX
+	push eax		                                 ;save accumulator eax to use in function
+	mov eax,0xA		                                 ;Hexadecimal LineFeed character
+	push eax		                                 ;save 0xA to stack
+	mov eax, esp	                                 ;mov 0xA location to stack  // esp points to last direction of stack or to eax
 	call sprint		                                 ;calls and prints LineFeed 
-	pop EAX			                                 ;get back 0xA
-	pop EAX			                                 ;get back original value of 0xA
+	pop eax			                                 ;get back 0xA
+	pop eax			                                 ;get back original value of 0xA
 	ret				                                 ;returns
 
 ;Function iprint (IntegerPrint)
@@ -63,88 +63,89 @@ iprint:
 	push ecx				                         ;save ecx to stack (counter)
 	push edx				                         ;save edx to stack (data)
 	push esi				                         ;save esi to stack (source index)
-	mov ecx, 0				                         ;count how many bytes we need
+	mov ecx,0				                         ;count how many bytes we need
 	
 dividirLoop:
 	inc ecx					                         ;increment ecx by 1
 	mov edx, 0 				                         ;clean edx
 	mov esi, 10				                         ;save 10 in esi, we will divide by 10
-	idiv esi				                         ;divide eax by esi, siempre divide a EAX lo que este en eax lo divide con el vqlor de esi	
-	add EDX, 48				                         ;agregamos el caracter 48 '0' 
-	push EDX				                         ;la representación en ASCII de nuestro numero, lo guarda en el stack como caracter
-	cmp EAX, 0 				                         ;se puede dividir mas el numero entero ?
+	idiv esi				                         ;divide eax by esi, siempre divide a eax lo que este en eax lo divide con el vqlor de esi	
+	add edx, 48				                         ;agregamos el caracter 48 '0' 
+	push edx				                         ;la representación en ASCII de nuestro numero, lo guarda en el stack como caracter
+	cmp eax, 0 				                         ;se puede dividir mas el numero entero ?
 	jnz dividirLoop			                         ;jump if not zero (salta si no es cero)
 
 imprimirLoop:
-	dec ECX					                         ;count backwards each byte in stack
-	mov EAX, ESP			                         ;stack pointer to eax
+	dec ecx					                         ;count backwards each byte in stack
+	mov eax, esp			                         ;stack pointer to eax
 	call sprint				                         ;call sprint function
-	pop EAX					                         ;remove last char from stack and send to eax
-	cmp ECX, 0 				                         ;print all of stack's bytes?
+	pop eax					                         ;remove last char from stack and send to eax
+	cmp ecx, 0 				                         ;print all of stack's bytes?
 	jnz imprimirLoop		                         ;there is no number to print
 	
-	pop ESI 				                         ;recover value of ESI
-	pop EDX					                         ;recover value of EDX
-	pop ECX                                          ;recover value of ECX
-	pop EAX 				                         ;recover value of EAX
+	pop esi 				                         ;recover value of esi
+	pop edx					                         ;recover value of edx
+	pop ecx                                          ;recover value of ecx
+	pop eax 				                         ;recover value of eax
 	ret
 
 iprintLF:
     call iprint                                      ;print out the numbers
-    push EAX                                         ;saves what's stored in EAX
+    push eax                                         ;saves what's stored in eax
 
-    mov EAX,0xA                                      ;copy line-feed to EAX
-    push EAX                                         ;saves line-feed in the stack
-    mov EAX, ESP                                     ;copies stack's pointer
+    mov eax,0xA                                      ;copy line-feed to eax
+    push eax                                         ;saves line-feed in the stack
+    mov eax, esp                                     ;copies stack's pointer
     call sprint                                      ;print out the line-feed
-    pop EAX                                          ;removes line-feed from stack
-    pop EAX                                          ;recover data that we had
+    pop eax                                          ;removes line-feed from stack
+    pop eax                                          ;recover data that we had
     ret                                              ;returns
 
 ;==========================
 ; Converts int to ASCII
 ;==========================
 atoi:
-    push EBX                                         ;preserve EBX
-    push ECX                                         ;preserve ECX
-    push EDX                                         ;preserve EDX
-    push ESI                                         ;preserves ESI
-    mov ESI, EAX                                     ;number to convert to ESI
-    mov EAX, 0                                       ;initialize EAX in 0
-    mov ecx, 0                                       ;initialize ECX in 0
+    push ebx                                         ;preserve ebx
+    push ecx                                         ;preserve ecx
+    push edx                                         ;preserve edx
+    push esi                                         ;preserves esi
+    mov esi, eax                                     ;number to convert to esi
+    mov eax, 0                                       ;initialize eax in 0
+    mov ecx, 0                                       ;initialize ecx in 0
 
     loopmult:                                        ;loop multiplication
 
-    xor EBX, EBX                                     ;reset EBX to 0
-    mov BL, [ESI + ECX]
+    xor ebx, ebx                                     ;reset ebx to 0
+    mov bl, [esi + ecx]
 
-    cmp BL, 48                                       ;compare with ASCII '0'
-    jl finalizado                                    ;if less, jump to finalizado
+    cmp bl, 48                                       ;compare with ASCII '0'
+    jl finalized                                     ;if less, jump to finalized
 
-    cmp BL, 10                                       ;compare with linefeed
-    je finalizado                                    ;if equal, jump to finalizado
+    cmp bl, 57                                       ;compare with ascci '9'
+    jg finalized                                     ;if greater, finalized
 
-    cmp BL, 0                                        ;compare with null character
-    jz finalizado                                    ;if zero, jump to finalizado
+    cmp bl, 10                                       ;compare with linefeed
+    je finalized                                     ;if equal, jump to finalized
 
-    sub BL, 48                                       ;convert character to int
-    add EAX, EBX                                     ;add value of EBX
-    mov EBX, 10                                      ;move decimal 10 to EBX
-    mul EBX                                          ;multiplies EAX x EBC
-    inc ECX                                          ;increment ECX (counter)
+    cmp bl, 0                                        ;compare with null character
+    jz finalized                                     ;if zero, jump to finalized
+
+    sub bl, 48                                       ;convert character to int
+    add eax, ebx                                     ;add value of ebx
+    mov ebx, 10                                      ;move decimal 10 to ebx
+    mul ebx                                          ;multiplies eax x EBC
+    inc ecx                                          ;increment ecx (counter)
     jmp loopmult                                     ;keep looping the multiplication loop
 
-    finalizado:
-    mov ebx,10 	                                     ; move decimal 10 to EBX
-    div ebx		                                     ; divide EBX by 10
-    pop esi 	                                     ; get ESI
+    finalized:
+    mov ebx,10 	                                     ; move decimal 10 to ebx
+    div ebx		                                     ; divide ebx by 10
+    pop esi 	                                     ; get esi
     pop edx		                                     ; get edx
     pop ecx		                                     ; get ecx
     pop ebx 	                                     ; get ebx
     ret
-    
-
 
 quit:
-    mov EAX, sys_exit                                ;sys_exit
+    mov eax, sys_exit                                ;sys_exit
     int 0x80                                         ;kernel call
